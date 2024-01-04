86f1dbac0db2d543d08a9267c9a25fd72cac9798
jayallen <envicomm.jay@gmail.com>
2024-01-04 14:27:30 +0800

    wip

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("Hello World")
+
+
+// var variable = "helloworld"
+
+// variable = "jay"
+// console.log(variable)
+// let iLoveCoffee = "Helloworld"
+// console.log("\t", iLoveCoffee)
+
+
+let Var = "10"
+let PM1 = "10"
+console.log(Var + PM1)
+console.log(Var - PM1)
+
