7aa7187ed6cfba367eb91930bb1f0d3e2f251b1d
jayallen <envicomm.jay@gmail.com>
2024-01-05 14:56:56 +0800

    jayallen

--- jayallen.js
+++ jayallen.js
@@ -1,4 +1,4 @@
-
+//Data Types
 let fName = "JayAllen"
 console.log(fName)
 
@@ -17,9 +17,27 @@ let cmHeight = 173
 var mheight  = 100
 console.log(cmHeight / mheight)
 
+//Coercion
 let Fname = "Jayallen"
-
 console.log(Fname+myAge)
 
+//Math Operators
 console.log(myAge + 2023)
 console.log(myAge * 2023)
+
+//Functions
+let hello = "Hello WORLD"
+console.log(hello)
+let number1 =  5
+let number2 =  5
+var sum = number1 + number2
+var product = number1 * number2
+console.log(sum)
+console.log(product)
+
+let greet = "Hello"
+console.log(greet,"Jay")
+
+
+
+
